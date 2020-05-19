43dee274d2bfb932765df4053acd589a4ca0175a
akahn16 <akahn16@outlook.com>
2020-05-19 11:47:40 -0400

    Create ClearTag.ps1

---
+++ ClearTag.ps1
@@ -0,0 +1,16 @@
+#Clear Tag
+$creds = Get-Credential
+$OK = "OK"
+$EPOError = "Error"
+$EPOTAG="<Place_Tag_Here>"
+$ComputerNames="System1,System2,System3"
+$Uri = "https://EPO1:8443/remote/system.clearTag?names=$ComputerNames&tagName=$EPOTAG"
+$output = Invoke-RestMethod -Uri $Uri -Credential $creds
+if($output -match $OK)
+{
+write-host "Success"
+}
+if($output -match $EPOError)
+{
+write-host "Error"
+}
